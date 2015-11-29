32b9d35935a0d8596fd3f6344c52a60239d75640
neochief <neochief@shvetsgroup.com>
2015-11-29 23:56:20 +0200

    Moved around lexer files.

--- src/com/squirrelplugin/lexer/build_lexer.sh
+++ src/com/squirrelplugin/lexer/build_lexer.sh
@@ -1,1 +1,3 @@
-~/idea/tools/lexer/jflex-1.4/bin/jflex --skel ~/idea/tools/lexer/idea-flex.skeleton --charat --nobak SquirrelLexer.flex -d ../../../../gen/com/squirrelplugin/lexer/
+#!/bin/bash
+
+~/idea/tools/lexer/jflex-1.4/bin/jflex --skel ~/idea/tools/lexer/idea-flex.skeleton --charat --nobak src/com/squirrelplugin/lexer/SquirrelLexer.flex -d gen/com/squirrelplugin/lexer/
